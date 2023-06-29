f3007361a7f4258838fb771b69d014e7fc91bc9a
william-valenduc <william.valenduc@orange.fr>
2023-06-29 15:13:54 +0200

    re-init
    restored php version

--- server/resources/js/header.d.ts
+++
@@ -1,9 +0,0 @@
-declare const THEME: {
-    [key: string]: {
-        background: string;
-        name: string;
-        textColor: string;
-    };
-};
-
-declare function seTheme(theme: string): void;
