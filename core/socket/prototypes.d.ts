fbc4b1f07f2a3762c10aa903e951a4e68f1b99b7
Darco2903 <114527594+Darco2903@users.noreply.github.com>
2024-12-01 02:52:14 +0100

    core types

--- core/socket/prototypes.d.ts
+++ core/socket/prototypes.d.ts
@@ -1,16 +1,14 @@
 import { Socket } from "socket.io";
 import { User } from "auth-api";
 
-type Cookies = {
-    [key: string]: string;
-    session_id: string;
-};
+import { Cookies } from "../types";
 
 declare module "socket.io" {
     interface Socket {
         getCookies(): Cookies;
         user: {
             session_id: string;
+            level: number;
             info: User;
         };
     }
