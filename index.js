51bdc513a3bb4a1b7cbf1c0808c961cad89e2b85
william-valenduc <william.valenduc@orange.fr>
2024-12-31 00:12:08 +0100

    cwd temp fix

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+////////////////////////////////////////
+// TEMP - until relative paths are fixed
+// check if cwd is correct
+if (__dirname !== process.cwd()) {
+    console.error("CWD is not correct");
+    process.exit(1);
+}
+////////////////////////////////////////
+
 const { colors, logInfo, logDebug, setDevMode, setDebugMode } = require("logger");
 
 const server = require("./core/server/index");
@@ -9,29 +18,6 @@ const { DEBUG, DEV_MODE } = require("./core/utils");
 const proxy = require("./core/server/proxy");
 
 const { listen, port, SERVER_PATH, WSAllowedOrigins, authServerOrigin, CLOUDFRONT_ID, authorizeNonCloudfront } = require("./config/server.json");
-
-// (async () => {
-//     await utils.printLog(colors.magenta("Starting server..."));
-//     if (utils.DEV_MODE) await utils.printLog(colors.magenta.magenta("------- DEV MODE -------"));
-//     await utils.printDebug("Debug mode enabled");
-//     await utils.printLog(colors.gray("-".repeat(24)));
-
-//     await utils.printDebug("Connecting to database...");
-//     await db.connect().catch((err) => {
-//         utils.printLog(colors.red("Error connecting to database =>"), colors.magenta(err.message));
-//         process.exit(1);
-//     });
-//     await utils.printLog(colors.green("Database connected"));
-
-//     await utils.printLog(
-//         colors.cyan("Proxy Server"),
-//         proxy.enabled ? colors.green("Enabled") : proxy.configOk ? colors.yellow("Disabled") : colors.red("Error")
-//     );
-
-//     server.listen(port, listen, async () => {
-//         await utils.printLog(`Server is listening ${colors.green(listen)}:${colors.yellow(port)}`);
-//     });
-// })();
 
 (async () => {
     setDebugMode(DEBUG);
