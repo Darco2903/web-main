310c64c57fd546fd7c924558694c068454f2a847
Darco2903 <114527594+Darco2903@users.noreply.github.com>
2025-08-10 04:38:20 +0200

    ssl path fix

--- server/src/server/app.ts
+++ server/src/server/app.ts
@@ -1,13 +1,18 @@
 import fs from "fs";
+import path from "path";
 import https from "https";
 import express from "express";
+import { fileURLToPath } from "url";
 
 import config from "../../config/express.json" with { type: "json" };
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const httpsOptions = {
-    key: fs.readFileSync(config.ssl.key, "utf8"),
-    cert: fs.readFileSync(config.ssl.cert, "utf8"),
-    ca: fs.readFileSync(config.ssl.ca, "utf8"),
+    key: fs.readFileSync(path.join(__dirname, "../", config.ssl.key), "utf8"),
+    cert: fs.readFileSync(path.join(__dirname, "../", config.ssl.cert), "utf8"),
+    ca: fs.readFileSync(path.join(__dirname, "../", config.ssl.ca), "utf8"),
 };
 
 export const app = express();
