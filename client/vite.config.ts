14666fd4505a251c79e881df8b0713e1e4157fc8
Darco2903 <114527594+Darco2903@users.noreply.github.com>
2025-08-18 15:14:59 +0200

    vue-router meta
    workspace update
    remove config
    vue-router meta

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -27,7 +27,6 @@ export default defineConfig({
             "@fonts": "/src/assets/fonts",
             "@icons": "/src/assets/icons",
             "@comp": "/src/components",
-            "@config": "/src/config",
             "@mod": "/src/modules",
             "@pages": "/src/pages",
             "@router": "/src/router",
